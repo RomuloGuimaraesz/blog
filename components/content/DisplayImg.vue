9a4d0bbd6fd54ae4d6020d77e8229a4c72af56f5
Rômulo Guimarães <romulo.fantomaz@gmail.com>
2024-07-20 21:59:47 -0300

    chore: publish my fourth post

---
+++ components/content/DisplayImg.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  name: 'DisplayImg',
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    imgSrc () {
+      return require(`~/assets/${this.src}`)
+    }
+  }
+}
+</script>
+
+<template>
+  <picture class="image">
+    <img
+      :src="imgSrc"
+      :alt="alt"
+      draggable="false"
+    >
+  </picture>
+</template>
+
+<style lang="scss" scoped>
+picture {
+  width: 300px;
+  height: 300px;
+  margin: 0;
+  display: flex;
+  img {
+    width: 100%;
+    height: 100%;
+    margin: auto;
+    object-fit: contain;
+  }
+}
+</style>
